64a2f198c8b1c94a00031fade3b8b8446a6d6839
jackWen525 <1434303814@qq.com>
2021-09-10 10:08:27 +0800

    扁平化数组

--- src/util/index.js
+++ src/util/index.js
@@ -11,15 +11,15 @@ function deepClone(target) {
     let clone;
     if (Array.isArray(target)) {
         clone = []
-    }else {
+    } else {
         clone = {}
     }
     for (const key in target) {
-        
+
         if (Object.hasOwnProperty.call(target, key)) {
 
             clone[key] = deepClone(target[key]);
-            
+
         }
     }
     return clone;
@@ -33,9 +33,31 @@ function deepClone(target) {
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+/**
+ * 
+ * @param {*} array 扁平化数据
+ * @returns 返回一维数组
+ */
+function arrayFlattening(array) {
+    const falarr = [];
+    for (const key in array) {
 
+        if (Object.hasOwnProperty.call(array, key)) {
+
+            if (Array.isArray(array[key])) {
+                falarr.push(...arrayFlattening(array[key]))
+            } else {
+                falarr.push(array[key]);
+            }
+
+
+        }
+    }
+    return falarr
+}
 export default {
     deepClone,
-    getRandom
+    getRandom,
+    arrayFlattening
 }
 
